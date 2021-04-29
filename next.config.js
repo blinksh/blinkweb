18c052fbf9584eeac3288991f51551087edb2cc3
Yury Korolev <yury.korolev@gmail.com>
2021-04-29 13:14:26 +0300

    Update deps

--- next.config.js
+++ next.config.js
@@ -3,23 +3,26 @@ const withMDX = require("@zeit/next-mdx")({
 });
 
 module.exports = withMDX({
+  future: {
+    webpack5: true,
+  },
   pageExtensions: ["js", "ts", "tsx", "md", "mdx"],
   webpack: (config, { dev, isServer }) => {
     if (!dev && isServer) {
       // we're in build mode so enable shared caching for the GitHub API
       process.env.USE_CACHE = "true";
 
-      const originalEntry = config.entry;
+      // const originalEntry = config.entry;
 
-      config.entry = async () => {
-        const entries = { ...(await originalEntry()) };
+      // config.entry = async () => {
+      //   const entries = { ...(await originalEntry()) };
 
-        // These scripts can import components from the app and use ES modules
-        // entries['./scripts/build-rss.js'] = './scripts/build-rss.js';
-        entries["./scripts/index-docs.js"] = "./scripts/index-docs.js";
+      //   // These scripts can import components from the app and use ES modules
+      //   // entries['./scripts/build-rss.js'] = './scripts/build-rss.js';
+      //   // entries["./scripts/index-docs.js"] = "./scripts/index-docs.js";
 
-        return entries;
-      };
+      //   return entries;
+      // };
     }
 
     return config;
