38531e4d4203ae9365fc7f16970aaeb130b21d00
Carlos Cabanero <carlos@blink.sh>
2023-02-16 03:03:34 +0000

    Blink Build ToS and new PP

--- pages/pp.tsx
+++ pages/pp.tsx
@@ -2,27 +2,90 @@ import Head from 'next/head'
 import SimpleLayout from '../components/SimpleLayout'
 
 const PP = () => (
-    <SimpleLayout>
-      <Head>
-        <title>Blink Shell &amp; Code Editor Pivacy Policy</title>
-        <meta
-          name="keywords"
-          content="blink, shell, terminal, mosh, ios, ssh, emulator, code editor"
-        />
-      </Head>
-      <h2>The Easiest to Understand Privacy Policy</h2>
-      <p>
-        Blink Shell app doesn't store your data. We don't even have where.
-      </p>
-      <p>
-        If you enable iCloud, the information of your hosts will be stored within iCloud conveniently protected by Apple. Even in that scenario, your keys or other protected data will never leave your device.
-      </p>
-        If we were to change this in the future, we will always allow you to opt-in.
-      <p>
-        That's all there is to understand. That was easy.
-      </p>
-    </SimpleLayout>
-  )
+  <SimpleLayout>
+    <Head>
+      <title>Blink Shell, Build &amp; Code Privacy Policy</title>
+      <meta
+        name="keywords"
+        content="blink, shell, terminal, mosh, ios, ssh, emulator, code editor"
+      />
+    </Head>
+    <h1 id="privacy-policy">Privacy Policy</h1>
+    <p>This Privacy Policy explains how Blink Shell, Inc. uses the personal data we collect from you from using our websites, software, and services (Services). The most effective method of safeguarding your data is to avoid having it in the first place, which is why we always strive to limit data requirements to what is essential to operate each Service, as outlined in this document. By using our Services, you consent to the data practices described in this statement.</p>
+    <h2 id="what-you-provide-to-us">What you provide to us</h2>
+    <p><em><strong>Blink Shell</strong></em> Usage of our terminal application does not request any information from the User.</p>
+    <p><em><strong>Blink Build</strong></em></p>
+    <p><em>Services Content</em>. The Service includes a Personal Cloud Disk with your files, software, data, settings and so on. The Service is designed to host and serve those files to create personalized computing environments.</p>
+    <p><em>Account information</em>. We use your email to have a direct way to contact in case of issues with the system, and other account preferences like geographical region to create the computing environments according to your needs.</p>
+    <p><em>Communications.</em> If you contact us directly for support or other reasons, we may receive additional information such as name, email and other contents of the message and information you choose to provide.</p>
+    <h2 id="what-we-collect-and-why">What we collect and why</h2>
+    <p><em><strong>Blink Shell and Blink Build</strong></em></p>
+    <p><em>Anonymous Information.</em> Blink Shell may perform anonymous web requests for feature like What’s New, Discovery and so on. Blink Build will perform authenticated requests when you perform operations within the Service (like starting machines, etc…). Every web request will always contain IPs, web browser type or device information.</p>
+    <p><em>Purchases.</em> We collect purchase history in order to grant the Services you are entitled to.</p>
+    <p><em><strong>Blink Build</strong></em></p>
+    <p><em>Logs.</em> We collect logs tied to your user ID in order to provide support. Eventually, logs may be anonymized to generate usage reports and other analytics that may be useful to run the Service.</p>
+    <h2 id="how-we-share-it">How we share it</h2>
+    <p>We don’t sell, rent or lease your information with any third parties.</p>
+    <p>In order to provide the Service, we use trusted partners to host our platform. We may share parts of your data with them as processors and they all comply proper privacy and security requirements (Data Processing Agreements), as can be seen in the next section.</p>
+    <p>We may disclose your personal information, without notice, if required to do so by law or in the good faith belief that such action is necessary to: (a) conform to the edicts of the law or comply with legal process served on Blink Shell Inc, website or Services; (b) protect and defend the rights or property of Blink Shell Inc; and/or (c) act under exigent circumstances to protect the personal safety of users of the Blink Shell Inc, or the public.</p>
+    <h2 id="how-we-store-it">How we store it</h2>
+    <p>Our applications and Services may use trusted partners to provide specific features. These providers act as data processors (under GPDR terms) with your information, and are subject themselves to legal obligations that we have summarized below.</p>
+    <p><em><strong>Blink Shell</strong></em></p>
+    <table>
+      <thead>
+        <tr>
+          <th>RevenueCat</th>
+          <th>USA</th>
+          <th>AppStore Purchases</th>
+          <th><a href="https://www.revenuecat.com/dpa/">https://www.revenuecat.com/dpa/</a></th>
+        </tr>
+      </thead>
+    </table>
+    <p><em><strong>Blink Build</strong></em></p>
+    <table>
+      <thead>
+        <tr>
+          <th>DigitalOcean</th>
+          <th>User Selected Regions</th>
+          <th>Cloud Storage and Cloud Computing</th>
+          <th><a href="https://www.digitalocean.com/legal/data-processing-agreement">https://www.digitalocean.com/legal/data-processing-agreement</a></th>
+        </tr>
+      </thead>
+      <tbody><tr>
+        <td>DigitalOcean</td>
+        <td>USA</td>
+        <td>Cloud Infrastructure</td>
+        <td><a href="https://www.digitalocean.com/legal/data-processing-agreement">https://www.digitalocean.com/legal/data-processing-agreement</a></td>
+      </tr>
+        <tr>
+          <td>Google Cloud</td>
+          <td>USA</td>
+          <td>Cloud Infrastructure</td>
+          <td><a href="https://cloud.google.com/terms/data-processing-addendum">https://cloud.google.com/terms/data-processing-addendum</a></td>
+        </tr>
+        <tr>
+          <td>Auth0</td>
+          <td>USA</td>
+          <td>Authentication</td>
+          <td><a href="https://auth0.com/docs/secure/data-privacy-and-compliance/gdpr">https://auth0.com/docs/secure/data-privacy-and-compliance/gdpr</a></td>
+        </tr>
+        <tr>
+          <td>RevenueCat</td>
+          <td>USA</td>
+          <td>Purchases</td>
+          <td><a href="https://www.revenuecat.com/dpa/">https://www.revenuecat.com/dpa/</a></td>
+        </tr>
+      </tbody></table>
+    <h2 id="security">Security</h2>
+    <p>Blink Shell Inc, uses various security technologies and measures to protect user information from unauthorized access, use, or disclosure. However, while they strive to protect user data, no service can guarantee complete security, and hence we cannot guarantee that unauthorized access, hacking, data loss or data breach will never occur.</p>
+    <h2 id="right-to-deletion">Right to deletion</h2>
+    <p>The Service allows you to request the deletion of your information, which can be done easily through our application. Please note that the deletion process may take several days to complete, depending on factors such as outstanding transactions.</p>
+    <h2 id="changes">Changes</h2>
+    <p>Please review this Privacy Policy periodically as it may be subject to modifications. Revisions to this Privacy Policy will be published on our websites. If we make material changes to the ways in which we previously used or shared your personal data through our Services, we will inform you through our Services, on this page, through our application, or through other means of communication.</p>
+    <h2 id="contact">Contact</h2>
+    <p>We welcome questions or concerns regarding this Privacy Policy, please email us at: <a href="mailto:hello@blink.sh">hello@blink.sh</a>. If you feel that we do have not addressed your concern in a satisfactory manner, you may contact your local data protection authority.</p>
+  </SimpleLayout>
+)
 
 
 export default PP
